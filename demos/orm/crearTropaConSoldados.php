efc60db8bcdd12dff8756008f35139ef3b115e29
ndiazpai <ndiazpai@sirius.lan>
2015-06-19 12:26:36 -0300

    ORM
    Se agrega el mapeo de las asociaciones entre tropa y soldados

--- demos/orm/crearTropaConSoldados.php
+++ demos/orm/crearTropaConSoldados.php
@@ -1,19 +1,13 @@
 <?php
-// create_product.php
 require_once "bootstrap.php";
 
 $tropa = new Tropa();
 $tropa->setNombre("Compania 123");
 
+$tropa->asignarSoldado($entityManager->find("Personaje", 1));
+$tropa->asignarSoldado($entityManager->find("Personaje", 4));
+
 $entityManager->persist($tropa);
-
-$soldado = $entityManager->find("Personaje", 2);
-$soldado->setTropa($tropa);
-$entityManager->persist($soldado);
-
-$soldado = $entityManager->find("Personaje", 3);
-$soldado->setTropa($soldado);
-
 $entityManager->flush();
 
 echo "La tropa fue creada y asignada con el ID:" . $tropa->getId() . "\n";
